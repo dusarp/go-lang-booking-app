5f10946da2f46e2f763e524f53be6e65a51500a6
Arpad Dusa <dusarp@gmail.com>
2025-12-19 19:02:04 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("We are excited to have you here today.")
+	fmt.Println("Get your tickets here to attend.")
+
+}
