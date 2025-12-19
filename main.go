3c0f3beb753dabb7d03975f098538e99754b640c
Arpad Dusa <dusarp@gmail.com>
2025-12-19 19:30:41 +0100

    add printf

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName string = "Go Conference"
+	const conferencTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to our", conferenceName, "booking application")
 	fmt.Println("We are excited to have you here today.")
-	fmt.Println("Get your tickets here to attend.")
+	fmt.Println("Get your tickets here to attend. ")
+	fmt.Println("We have a total of", conferencTickets, "tickets and", remainingTickets, "are still available.")
 
 }
