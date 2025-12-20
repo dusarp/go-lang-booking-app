1bfcc583c98b0457e3fa270c5552ff9b04c47ac9
Arpad Dusa <dusarp@gmail.com>
2025-12-20 07:39:23 +0100

    add comment

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
+// main is the entry point for the application (ez egy komment)
 func main() {
 
 	var conferenceName string = "Go Conference"
 	const conferencTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application")
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Println("We are excited to have you here today.")
 	fmt.Println("Get your tickets here to attend. ")
-	fmt.Println("We have a total of", conferencTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferencTickets, remainingTickets)
 
 }
